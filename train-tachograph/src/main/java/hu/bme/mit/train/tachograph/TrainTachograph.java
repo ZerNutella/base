da0713f384499eac0ade674dcae1b21e1b35304d
ZerNutella <bendi.bodolai@gmail.com>
2022-02-21 20:46:57 +0100

    position now public

--- train-tachograph/src/main/java/hu/bme/mit/train/tachograph/TrainTachograph.java
+++ train-tachograph/src/main/java/hu/bme/mit/train/tachograph/TrainTachograph.java
@@ -3,7 +3,7 @@ package hu.bme.mit.train.tachograph;
 import com.google.common.collect.Table;
 
 public class TrainTachograph{
-	private Table<String,String,Integer> joystickPosition;
+	public Table<String,String,Integer> joystickPosition;
 	
 	public TrainTachograph(String ct, String rs, Integer position) {
 		joystickPosition.put(ct,rs,position);
