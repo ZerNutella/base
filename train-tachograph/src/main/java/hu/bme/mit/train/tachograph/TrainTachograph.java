75ed332a90a247390c9d07d11ba2d4342eb3cf44
ZerNutella <bendi.bodolai@gmail.com>
2022-02-21 20:50:24 +0100

    null exception fixed by initializing the position table

--- train-tachograph/src/main/java/hu/bme/mit/train/tachograph/TrainTachograph.java
+++ train-tachograph/src/main/java/hu/bme/mit/train/tachograph/TrainTachograph.java
@@ -6,6 +6,8 @@ public class TrainTachograph{
 	public Table<String,String,Integer> joystickPosition;
 	
 	public TrainTachograph(String ct, String rs, Integer position) {
+		joystickPosition = new Table<String,String,Integer>();
 		joystickPosition.put(ct,rs,position);
 	}
+
 }
